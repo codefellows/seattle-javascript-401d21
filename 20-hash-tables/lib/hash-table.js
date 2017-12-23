678f0a48e8f22e0975449f1c8aa51b7004af1e4a
Vinicio <vinicio@codefellows.com>
2017-12-22 16:03:31 -0800

    day 18

--- 20-hash-tables/lib/hash-table.js
+++ 20-hash-tables/lib/hash-table.js
@@ -1,21 +1,32 @@
 'use strict';
+/* Hash Table
+  references to linked list
 
-const LinkedList = require('./linked-list');
+  Values
+  Array called buckets
+    array is going to have a fixed size
+  capacity 
 
-class HashTable{
-  constructor(capacity = 1024){
+
+  Operations
+  hash function
+  set
+  get
+  remove
+*/
+const LinkedList = require('./linked-list.js');
+
+module.exports = class HashTable{
+  constructor(capacity= 1024){
     this._capacity = capacity;
     this._buckets = new Array(capacity);
   }
 
-  // vinicio - this function will be very prone to collisions,
-  //           and it's not suited for production
-  _generateHash(key) {
+  _generateHash(key){
     if(typeof key !== 'string')
-      throw new TypeError('_ERROR_ key should be a string');
-    
+      throw new TypeError('__HASTABLE_ERROR_ key should be a string');
     let rawHash = 0;
-
+    
     for(let i in key){
       rawHash += key.charCodeAt(i);
     }
@@ -23,40 +34,60 @@ class HashTable{
     return rawHash % this._capacity;
   }
 
-  // 'gregor', 'cat'
-  // object.gregor = 'cat';
   set(key,htValue){
+    // calculate hash
+    // check if bucket is empty
+    // update if neccesary
+    // create a new linked list if not (handle collisions)
     let hash = this._generateHash(key);
 
-    // vinicio - the bucket is empty
-    if(!this._buckets[hash]){
+    if(!this._buckets[hash]){//vinicio - create a new linked list if there is nothing
       this._buckets[hash] = new LinkedList({key,htValue});
       return this;
     }
-    // vinicio - the bucket is NOT empty
+
     let node = this._buckets[hash].find(node => node.value.key === key);
 
     if(node){
-      node.value.htValue = htValue; // vinicio - updating the value
+      node.value.htValue = htValue;
       return this;
     }
-    // vinicio - if we are not updating a key, we create a new element in the linked list
     this._buckets[hash].append(new LinkedList({key,htValue}));
     return this;
   }
-
+  
   get(key){
+    //get hash
+    //check if bucket is empty
+    // return either the value or undefined
     let hash = this._generateHash(key);
+    if(!this._buckets[hash])
+      return; //vinicio - return undefined;
 
-    if(!this._buckets[hash]){
-      return undefined;
-    }
-    // vinicio - dealing with collisions
+    // vinicio - checkig if key is present wit existing hash
     let node = this._buckets[hash].find(node => node.value.key === key);
 
     if(node)
       return node.value.htValue;
-    else
-      return undefined;
   }
-}
+
+  delete(key){
+    //get hash
+    //check if buckets is empty
+    //find node in linked list
+    //  remove from linked list if present
+    let hash = this._generateHash(key);
+
+    if(!this._buckets[hash])
+      return false;
+
+    let node = this._buckets[hash].find(node => node.value.key === key);
+
+    if(node){
+      this._buckets[hash] = this._buckets[hash].remove(node);
+      return true;
+    }
+    return false;
+  }
+
+};
