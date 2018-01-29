66ebd319e0eca422af7fc77fb235bdf3fd4cf86b
Scott Schmidt <sjschmidt44@gmail.com>
2018-01-29 15:16:40 -0800

    update class 11 demo code to include the correct validation for handling 400 and 404

--- back-end/11-express/demos/01-express-http/lib/error-handler.js
+++ back-end/11-express/demos/01-express-http/lib/error-handler.js
@@ -5,7 +5,7 @@ module.exports = function(err, res) {
 
   switch(true) {
     case msg.includes('validation error'): return res.status(400).send(`${err.name}: ${err.message}`)
-    case msg.includes('path error'): return res.status(404).send(`${err.name}: ${err.message}`)
+    case msg.includes('enoent'): return res.status(404).send(`${err.name}: ${err.message}`)
     default: return res.status(500).send(`${err.name}: ${err.message}`)
   }
 }
--- back-end/11-express/demos/01-express-http/model/note.js
+++ back-end/11-express/demos/01-express-http/model/note.js
@@ -4,7 +4,7 @@ const uuid = require('uuid/v4')
 
 module.exports = function Note(title, content) {
   return new Promise((resolve, reject) => {
-    if(!title || !content) return reject(new Error('Cannot create Note. Title and Content required.'))
+    if(!title || !content) return reject(new Error('Validation Error. Cannot create Note. Title and Content required.'))
     this._id = uuid()
     this.title = title
     this.content = content
